f62732aa64283c044712cf7a112abcdf8b654180
TheBunny737 <78348062+TheBunny737@users.noreply.github.com>
2021-12-20 14:52:04 +0700

    Delete Lab 02PB directory

--- Lab 02PB/LAB02-3.py
+++
@@ -1,8 +0,0 @@
-#Lab02-3 (Space Print)
-
-x = float(input())
-y = float(input())
-print("1234567890" * 3) #Do not use this, this cause - on ELAB
-print("Total Income {:+8.2f} bahts" .format(x))
-print("Expense      {:+8.2f} bahts" .format(y))
-print("Profit       {:08.2f} bahts" .format(x+y))
--- Lab 02PB/LAB02-6.py
+++
@@ -1,7 +0,0 @@
-#Lab02-6 (SwapStar)
-
-n = int(input())
-s1 = input()
-s2 = input()
-add = n % 2
-print((n//2) * (s1+s2) + (add * s1))
