7deea4db73d700eac2176c9a207d963c2b8cb2b5
TheBunny320 <terapat676@hotmail.com>
2021-12-21 07:51:18 +0700

    Lab04-5 NIT

--- Lab04PB/LAB04-5.py
+++ Lab04PB/LAB04-5.py
@@ -1,1 +1,16 @@
-#Blank
+#Lab04-5 NIT
+
+age = int(input("Enter your age: "))
+income = int(input("Enter your age: "))
+
+if age < 15 or age > 60:
+    result = "Invalid age."
+elif income <= 0:
+    result = "Invalid income"
+else:
+    if income > 0 and income <= 30000:
+        result = "Your negative income tax is {:.2f} Baht." .format(income * 0.2)
+    else:
+        result = "Your negative income tax is {:.2f} Baht." .format((80000 - income) * 0.12)
+
+print(result)
