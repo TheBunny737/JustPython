f94f5507e2a3909a14550c678f14b31d691c5072
TheBunny320 <terapat676@hotmail.com>
2021-12-25 11:18:05 +0700

    Fixed Lab07-2 Traingle alphabet 2

--- Lab07PB/Lab07-2.py
+++ Lab07PB/Lab07-2.py
@@ -7,9 +7,9 @@ if n <= 0 or n > 26:
     print("Invalid input, program terminates.")
 else:
     while i > 0:
-        j = i
-        while j > 0:
-            print(chr(64+j), end="")
-            j -= 1
+        j = 0
+        while j < i:
+            print(chr(65+j), end="")
+            j += 1
         print()
         i -= 1
