b566d7648d62420acb16874655342fc10930192f
TheBunny320 <terapat676@hotmail.com>
2021-12-25 11:49:51 +0700

    Lab07-5 Prime number

---
+++ Lab07PB/Lab07-5.py
@@ -0,0 +1,21 @@
+#Lab07-5 (Prime Number)
+
+devide = 0
+i = 1
+
+while True:
+    num = int(input("Enter a number: "))
+    if num == 0:
+        print("End of program, goodbye.")
+        break
+    elif num <= 1:
+        print("Ivalid input, try again.")
+    else:
+        while i <= num:
+            if num % i == 0:
+                devide += 1
+            i += 1
+        if devide == 2:
+            print("{:d} is a prime number." .format(num))
+        else:
+            print("{:d} is not a prime number." .format(num))
