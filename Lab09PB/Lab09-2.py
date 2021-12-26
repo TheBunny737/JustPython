40cda43c4c7471b7395ea6e3304ac10a976e2606
TheBunny320 <terapat676@hotmail.com>
2021-12-26 12:10:26 +0700

    Lab09-2 Factor Count

---
+++ Lab09PB/Lab09-2.py
@@ -0,0 +1,14 @@
+#Lab09-2 (Factor Count)
+
+def factor_count(n):
+    count = 0
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            count += 1
+        i += 1
+    return count
+
+n = int(input("Enter n: "))
+c = factor_count(n)
+print(c)
