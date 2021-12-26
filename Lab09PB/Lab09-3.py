41fb08d320fb50ff7abfef1260a239f44f04190a
TheBunny320 <terapat676@hotmail.com>
2021-12-26 12:14:56 +0700

    Lab09-3 Bactteria

---
+++ Lab09PB/Lab09-3.py
@@ -0,0 +1,11 @@
+#Lab09-3 (Bactteria)
+
+def nb_year(p0, percent, aug, p):
+    count = 0
+    while (p0 <= p):
+        p0 += p0 * (percent / 100) + aug
+        count += 1
+    return count
+
+print(nb_year(1000, 2, 30, 1150))
+print(nb_year(1500000, 0.25, 1000, 2000000))
